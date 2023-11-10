202795796c92168cf3bda911128787052707325d
Bailey Crowley <60710757+BaileyLC@users.noreply.github.com>
2023-11-10 10:14:00 -0700

    Update Osmia_dev16S.R
    just some formatting

--- R Scripts/Osmia_dev16S.R
+++ R Scripts/Osmia_dev16S.R
@@ -38,7 +38,11 @@
   sampleID <- samples$sampleID
   nesting_tube <- samples$nesting_tube
   sample_or_control <- samples$sample_or_control
-  sampleinfo <- data.frame(extractionID = extractionID, sample_type = sample_type, sampleID = sampleID, nesting_tube = nesting_tube, sample_or_control = sample_or_control)
+  sampleinfo <- data.frame(extractionID = extractionID, 
+                           sample_type = sample_type, 
+                           sampleID = sampleID, 
+                           nesting_tube = nesting_tube, 
+                           sample_or_control = sample_or_control)
   rownames(sampleinfo) <- samples.out
 
 # Format your data to work with phyloseq
@@ -83,13 +87,13 @@
   ps.neg <- prune_samples(sample_data(ps1)$sample_or_control == "control", ps1)
   
 # Calculate taxa abundance in samples from sample counts
-  ps.neg.presence <- transform_sample_counts(ps.neg, function(abund) 1*(abund > 0))
+  ps.neg.presence <- transform_sample_counts(ps.neg, function(abund) 1 * (abund > 0))
   
 # Make phyloseq object of presence-absence in true positive samples
   ps.pos <- prune_samples(sample_data(ps1)$sample_or_control == "sample", ps1)
   
 # Calculate taxa abundance in samples from sample counts
-  ps.pos.presence <- transform_sample_counts(ps.pos, function(abund) 1*(abund > 0))
+  ps.pos.presence <- transform_sample_counts(ps.pos, function(abund) 1 * (abund > 0))
   
 # Make data.frame of prevalence in positive and negative samples
   df.pres <- data.frame(prevalence.pos=taxa_sums(ps.pos.presence), 
@@ -302,16 +306,18 @@
   ggplot(data = y3, aes(x = sampleID, y = Abundance, fill = Family)) + 
     geom_bar(stat = "identity", position = "fill") + 
     scale_fill_manual(values = colors) +
-    facet_grid(~ sample_type, scale = "free", 
-               space = "free") +
+    facet_grid(~ sample_type, scale = "free", space = "free") +
     theme(legend.position = "right") +
     ylab("Relative abundance") + 
     ylim(0, 1.0) +
     xlab("Treatment") +
     theme_bw() + 
     theme(text = element_text(size = 16)) +
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
-    theme(legend.justification = "left", legend.title = element_text(size = 16, colour = "black"), legend.text = element_text(size = 14, colour = "black")) + 
+    theme(panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank()) + 
+    theme(legend.justification = "left", 
+          legend.title = element_text(size = 16, colour = "black"), 
+          legend.text = element_text(size = 14, colour = "black")) + 
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
     guides(fill = guide_legend(ncol = 2)) +
     ggtitle("Bacteria")
@@ -338,7 +344,8 @@
     xlab("Treatment") +
     theme_bw() + 
     theme(text = element_text(size = 16)) +
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
+    theme(panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank()) + 
     theme(legend.justification = "left", 
           legend.title = element_text(size = 16, colour = "black"), 
           legend.text = element_text(size = 14, colour = "black")) + 
@@ -357,8 +364,11 @@
     xlab("Treatment") +
     theme_bw() + 
     theme(text = element_text(size = 16)) +
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
-    theme(legend.justification = "left", legend.title = element_text(size = 16, colour = "black"), legend.text = element_text(size = 14, colour = "black")) + 
+    theme(panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank()) + 
+    theme(legend.justification = "left", 
+          legend.title = element_text(size = 16, colour = "black"), 
+          legend.text = element_text(size = 14, colour = "black")) + 
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
     guides(fill = guide_legend(ncol = 3)) +
     ggtitle("Bacteria")
