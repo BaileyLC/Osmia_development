e8a1a360ce10798cb63eecde1485b32cc4e6cdcd
Bailey Crowley <60710757+BaileyLC@users.noreply.github.com>
2024-02-23 21:18:02 -0700

    Update Osmia_dev_figs.R
    added more plots

--- R Scripts/Osmia_dev_figs.R
+++ R Scripts/Osmia_dev_figs.R
@@ -1,4 +1,4 @@
-##### Project: Osmia developmental microbiome
+##### Project: Osmia Developmental Microbiome
 
 #### Owners: Bailey Crowley & Robert N. Schaeffer
 
@@ -16,7 +16,7 @@
 ## Shannon index ----
 
 # Create plot
-  Osmia_dev_Shannon <- Osmia_dev_Shannon_bact + Osmia_dev_Shannon_fungi + plot_layout(ncol = 2, nrow = 1)
+  Osmia_dev_Shannon <- Osmia_dev_Shannon_bact + Osmia_dev_Shannon_fungi + plot_layout(ncol = 1, nrow = 2)
   Osmia_dev_Shannon
   
 # Save plot  
@@ -25,7 +25,7 @@
 ## Simpson index ----
 
 # Create plot
-  Osmia_dev_Simpson <- Osmia_dev_Simpson_bact + Osmia_dev_Simpson_fungi + plot_layout(ncol = 2, nrow = 1)
+  Osmia_dev_Simpson <- Osmia_dev_Simpson_bact + Osmia_dev_Simpson_fungi + plot_layout(ncol = 1, nrow = 2)
   Osmia_dev_Simpson
   
 # Save plot  
@@ -34,7 +34,7 @@
 ## Observed richness ----
   
 # Create plot
-  Osmia_dev_Observed <- Osmia_dev_Observed_bact + Osmia_dev_Observed_fungi + plot_layout(ncol = 2, nrow = 1)
+  Osmia_dev_Observed <- Osmia_dev_Observed_bact + Osmia_dev_Observed_fungi + plot_layout(ncol = 1, nrow = 2)
   Osmia_dev_Observed
   
 # Save plot  
@@ -42,23 +42,67 @@
   
 ## Rarefaction curves ----
 
+# All pollen and bee samples  
+  
 # Create plot
-  Osmia_dev_rare <- Osmia_dev_rare_bact + Osmia_dev_rare_fungi + plot_layout(ncol = 2, nrow = 1)
+  Osmia_dev_rare <- Osmia_dev_rare_bact + Osmia_dev_rare_fungi + plot_layout(ncol = 1, nrow = 2)
   Osmia_dev_rare
   
 # Save plot
   ggsave("Osmia_dev_rare.png", plot = Osmia_dev_rare)
   
-## PCoA plots ----
+# Only bee samples
   
 # Create plot
-  Osmia_dev_PCoA <- Osmia_dev_PCoA_bact + Osmia_dev_PCoA_fungi + plot_layout(ncol = 2, nrow = 1)
-  Osmia_dev_PCoA
+  Osmia_dev_rare_bee <- Osmia_dev_rare_bact_bee + Osmia_dev_rare_fungi_bee + plot_layout(ncol = 1, nrow = 2)
+  Osmia_dev_rare_bee
   
 # Save plot
-  ggsave("Osmia_dev_PCoA.png", plot = Osmia_dev_PCoA)
+  ggsave("Osmia_dev_rare_bee.png", plot = Osmia_dev_rare_bee)
+  
+## PCoA plots ----
+  
+# All pollen and bee samples  
+  
+# Relative abundance data  
+  
+# Create plot
+  Osmia_dev_PCoA_relabund <- Osmia_dev_PCoA_bact + Osmia_dev_PCoA_fungi + plot_layout(ncol = 2, nrow = 1)
+  Osmia_dev_PCoA_relabund
+  
+# Save plot
+  ggsave("Osmia_dev_PCoA_relabund.png", plot = Osmia_dev_PCoA_relabund)
+  
+# Rarefied data
+  
+# Create plot
+  Osmia_dev_PCoA_rare <- Osmia_dev_PCoA_bact_rare + Osmia_dev_PCoA_fungi_rare + plot_layout(ncol = 2, nrow = 1)
+  Osmia_dev_PCoA_rare
+  
+# Save plot
+  ggsave("Osmia_dev_PCoA_rare.png", plot = Osmia_dev_PCoA_rare)
 
-## Relative abundance by Family ----
+# Only bee samples
+  
+# Relative abundance data  
+  
+# Create plot
+  Osmia_dev_PCoA_relabund_bee <- Osmia_dev_PCoA_bact_bee + Osmia_dev_PCoA_fungi_bee + plot_layout(ncol = 2, nrow = 1)
+  Osmia_dev_PCoA_relabund_bee
+  
+# Save plot
+  ggsave("Osmia_dev_PCoA_relabund_bee.png", plot = Osmia_dev_PCoA_relabund_bee)
+  
+# Rarefied data
+  
+# Create plot
+  Osmia_dev_PCoA_rare_bee <- Osmia_dev_PCoA_bact_rare_bee + Osmia_dev_PCoA_fungi_rare_bee + plot_layout(ncol = 2, nrow = 1)
+  Osmia_dev_PCoA_rare_bee
+  
+# Save plot
+  ggsave("Osmia_dev_PCoA_rare_bee.png", plot = Osmia_dev_PCoA_rare_bee)
+  
+## Stacked Plot by Family ----
   
 # Create plot
   Osmia_dev_fam_relabund <- Osmia_dev_fam_relabund_bact + Osmia_dev_fam_relabund_fungi + plot_layout(ncol = 1, nrow = 2)
@@ -67,7 +111,7 @@
 # Save plot
   ggsave("Osmia_dev_fam_relabund.png", plot = Osmia_dev_fam_relabund)
   
-## Relative abundance by Genus ----
+## Stacked Plot by Genus ----
   
 # Create plot
   Osmia_dev_gen_relabund <- Osmia_dev_gen_relabund_bact + Osmia_dev_gen_relabund_fungi + plot_layout(ncol = 1, nrow = 2)
@@ -75,3 +119,12 @@
   
 # Save plot
   ggsave("Osmia_dev_gen_relabund.png", plot = Osmia_dev_gen_relabund)
+  
+## Stacked Plot by Top 15 Genera ----
+  
+# Create plot
+  Osmia_dev_gen_top15 <- Osmia_dev_15gen_relabund_bact + Osmia_dev_15gen_relabund_fung + plot_layout(ncol = 1, nrow = 2)
+  Osmia_dev_gen_top15
+  
+# Save plot
+  ggsave("Osmia_dev_gen_top15.png", plot = Osmia_dev_gen_top15, width = 15, height = 10, units = "in")
